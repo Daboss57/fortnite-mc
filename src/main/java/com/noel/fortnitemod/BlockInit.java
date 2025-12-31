72d74d78db2ed62725e9b899730d1605773ed026
Daboss57 <79070371+Daboss57@users.noreply.github.com>
2025-12-30 16:39:50 -0800

    added wall block

--- src/main/java/com/noel/fortnitemod/BlockInit.java
+++ src/main/java/com/noel/fortnitemod/BlockInit.java
@@ -3,6 +3,7 @@ package com.noel.fortnitemod;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MapColor;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredBlock;
@@ -13,21 +14,21 @@ public class BlockInit {
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(Fortnite.MODID);
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Fortnite.MODID);
 
-    // Wood Wall - simple block
+    // Wood Wall - thin wall (model makes it thin, not code)
     public static final DeferredBlock<Block> WOOD_WALL = BLOCKS.registerSimpleBlock(
             "wood_wall",
-            p -> p.mapColor(MapColor.WOOD).strength(0.5f).sound(SoundType.WOOD)
+            p -> p.mapColor(MapColor.WOOD).strength(0.5f).sound(SoundType.WOOD).noOcclusion()
     );
     public static final DeferredItem<BlockItem> WOOD_WALL_ITEM = ITEMS.registerSimpleBlockItem("wood_wall", WOOD_WALL);
 
-    // Wood Ramp - also just a simple block for now (we can make it look like stairs with model)
+    // Wood Ramp - simple block for now
     public static final DeferredBlock<Block> WOOD_RAMP = BLOCKS.registerSimpleBlock(
             "wood_ramp",
             p -> p.mapColor(MapColor.WOOD).strength(0.5f).sound(SoundType.WOOD)
     );
     public static final DeferredItem<BlockItem> WOOD_RAMP_ITEM = ITEMS.registerSimpleBlockItem("wood_ramp", WOOD_RAMP);
 
-    // Wood Floor - also just a simple block for now
+    // Wood Floor - simple block for now
     public static final DeferredBlock<Block> WOOD_FLOOR = BLOCKS.registerSimpleBlock(
             "wood_floor",
             p -> p.mapColor(MapColor.WOOD).strength(0.5f).sound(SoundType.WOOD)
